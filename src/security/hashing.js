d40c71478b91a06d54fe994e59db5dd99913e820
vpast <vpast.dev@gmail.com>
2023-04-04 14:29:59 +0400

    redirect from react-router-dom

--- src/security/hashing.js
+++
@@ -1,7 +0,0 @@
-var bcrypt = require('bcryptjs');
-
-const salt = bcrypt.genSaltSync(10);
-var hash = bcrypt.hashSync(registerData.password, salt);
-
-console.log(registerData.password)
-console.log(salt)
